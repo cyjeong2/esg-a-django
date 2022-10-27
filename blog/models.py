f2c3e63fef7174f8a0117b2d119f2f7d22805e85
cyjeong2 <jcyjjang95@gmail.com>
2022-10-27 11:56:56 +0900

    blog.Post 모델에 get_absolute_url 메서드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,3 @@
-from email import contentmanager
-from turtle import title
 from django.db import models
 
 class Post(models.Model) :
@@ -8,6 +6,9 @@ class Post(models.Model) :
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now = True)    
     
+    def get_absolute_url(self) :
+        # ToDO: 향후에는 장고의 URL Reverse 기능을 사용하기.
+        return f"/blog/{self.pk}/"
     
     def __str__(self):
         return f'[{self.pk}] {self.title}'
