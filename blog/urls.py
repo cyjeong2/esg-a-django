edc2beca356b127fa731ae7804a5afb9b52335f0
cyjeong2 <jcyjjang95@gmail.com>
2022-10-27 12:18:04 +0900

    22/10/27 점심먹기 전 저장

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    path('new/', views.post_new),
     path('', views.index), 
     path('<int:pk>/', views.single_post_page),
 ]
